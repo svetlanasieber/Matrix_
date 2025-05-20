225f3950a9eaf86ad39e4f4270dc9ad0bda70f42
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-05-20 20:18:53 +0200

    Update PrintDiagonalsOfSquareMatrix.java

--- PrintDiagonalsOfSquareMatrix.java
+++ PrintDiagonalsOfSquareMatrix.java
@@ -1,52 +1,43 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
-class PrintDiagonalsOfSquareMatrix {
+public class PrintDiagonalsOfSquareMatrix {
+
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
 
-
-        int n = Integer.parseInt(scanner.nextLine());
-
+        int n = scanner.nextInt();
         int[][] matrix = new int[n][n];
-
         fillMatrix(matrix, scanner);
 
-        printElementsOnPrimaryDiagonal(matrix);
-
+        printPrimaryDiagonal(matrix);
         System.out.println();
-
-        printElementsOnSecondaryDiagonal(matrix);
+ 
+        printSecondaryDiagonal(matrix);
     }
 
-    private static void printElementsOnSecondaryDiagonal(int[][] matrix) {
+    private static void printSecondaryDiagonal(int[][] matrix) {
 
-        for (int col = 0; col <= matrix.length - 1; col++) {
-            for (int row = 0; row <= matrix[0].length - 1; row++) {
-                int currentElement = matrix[row][col];
-                if (row + col == matrix.length - 1) {
-                    System.out.print(currentElement + " ");
-                }
-            }
+        int col = 0;
+        for (int row = matrix.length - 1; row >= 0 ; row--) {
+            System.out.print(matrix[row][col] + " ");
+            col++;
         }
     }
 
-    private static void printElementsOnPrimaryDiagonal(int[][] matrix) {
-        for (int row = 0; row <= matrix.length - 1; row++) {
-            for (int col = 0; col <= matrix[0].length - 1; col++) {
-                int currentElement = matrix[row][col];
+    private static void printPrimaryDiagonal(int[][] matrix) {
 
-                if (row == col) {
-                    System.out.print(currentElement + " ");
-                }
-            }
+        for (int row = 0; row < matrix.length; row++) {
+            System.out.print(matrix[row][row] + " ");
         }
     }
 
     private static void fillMatrix(int[][] matrix, Scanner scanner) {
-        for (int row = 0; row <= matrix.length - 1; row++) {
-            matrix[row] = Arrays.stream(scanner.nextLine().split("\\s+"))
-                    .mapToInt(Integer::parseInt).toArray();
+
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                matrix[row][col] = scanner.nextInt();
+            }
         }
     }
 }
